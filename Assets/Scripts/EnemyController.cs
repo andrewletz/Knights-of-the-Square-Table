3685f5a6b113500dd1934b2d693a5cca3a04d851
Josh Tanke <jtanke@uoregon.edu>
2019-05-03 00:13:47 -0700

    poc

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Death() {
+    	anim.SetBool("isDed", true);
+    	this.gameObject.GetComponent<Rigidbody>().detectCollisions = false;
+    	StartCoroutine(deathCo());
+    }
+
+    IEnumerator deathCo() {
+    	yield return new WaitForSeconds(1.5f);
+    	this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/PlayerHit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+    	if (other.CompareTag("mortal")) {
+    		other.GetComponent<EnemyController>().Death();
+    		Debug.Log("Called");
+    	}
+    }
+}
