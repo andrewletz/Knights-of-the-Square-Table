da014384073ec43c74544f71ad4311374680fbe4
Christopher Johnson <Jens@Christophers-MacBook-Pro-2.local>
2019-05-02 21:49:06 -0700

    merging changes

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject mainPlayer;
+    public float playerDistance = 2.0f, height = 4.0f;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start(){
+        transform.position = new Vector3(mainPlayer.transform.position.x, height, mainPlayer.transform.position.z - playerDistance);
+        offset = transform.position - mainPlayer.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = mainPlayer.transform.position + offset;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,38 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 10.0f;
+    public float speed = 10.0f;
 
-	private Vector3 forward;
-	private Vector3 right;
+    private Rigidbody rigidBody;
 
-	void Start() {
-		forward = Camera.main.transform.forward;
-		forward.y = 0;
-		forward = Vector3.Normalize(forward);
-		right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
-	}
+    void Start() {
+        rigidBody = GetComponent<Rigidbody>();
+    }
 
-	void Update() {
-		move();
-	}
+    void FixedUpdate() {
+        move();
+    }
 
-	void move() {
-		Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-		Vector3 rightMovement = right * speed * Time.deltaTime * Input.GetAxisRaw("Horizontal");
-		Vector3 upMovement = forward * speed * Time.deltaTime * Input.GetAxisRaw("Vertical");
-
-		Vector3 mouse = Input.mousePosition;
-		Vector3 playerPos = Camera.main.WorldToScreenPoint(transform.position);
-		Vector2 relativePos = new Vector2(mouse.x - playerPos.x, mouse.y - playerPos.y);
-		float angle = Mathf.Atan2(relativePos.y, relativePos.x) * Mathf.Rad2Deg;
-		if (angle < 0) angle += 360;
-		
-
-		//transform.rotation = Quaternion.Euler(transform.rotation.x, -angle, transform.rotation.z);
-
-		transform.position += rightMovement;
-		transform.position += upMovement;
-	}
-
+    void move() {
+        Vector3 horizontalMovement = new Vector3(speed * Time.deltaTime * Input.GetAxisRaw("Horizontal"), 0, 0);
+        Vector3 verticalMovement = new Vector3(0, 0, speed * Time.deltaTime * Input.GetAxisRaw("Vertical"));
+        rigidBody.MovePosition(transform.position + horizontalMovement + verticalMovement);
+    }
 }
