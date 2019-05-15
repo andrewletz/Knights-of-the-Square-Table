be1ff168ea27e3e1a4a9a355c88be0fd1536f3ff
andrewletz <andrewletz@gmail.com>
2019-05-14 22:04:39 -0700

    Created new player controller with floating weapon Basic swipe animation implemented New player sprite added

---
+++ Assets/Scripts/PlayerControllerFloating.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public enum PlayerState {
+// 	Walk,
+// 	Attack,
+// 	Interact
+// }
+
+public class PlayerControllerFloating : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public PlayerState currentState;
+
+	private Rigidbody rigidBody;
+	private Animator animator;
+
+	private GameObject weapon;
+
+	void Start() {
+		animator = GetComponentInChildren<Animator>();
+		rigidBody = GetComponent<Rigidbody>();
+
+		// get our weapon object once at start time
+		for (int i = 0; i < transform.childCount; i++) {
+			Transform currentTransform = transform.GetChild(i).transform;
+			if (currentTransform.name == "Weapon") {
+				this.weapon = currentTransform.gameObject;
+			}
+		}
+	}
+
+	void Update() {
+        if (Input.GetButtonDown("attack") && currentState != PlayerState.attack) {
+            //StartCoroutine(AttackCo());
+            weapon.SendMessage("StartAttackAnimation");
+        }
+
+        Vector3 mousePos = Input.mousePosition;
+        //Debug.Log(mousePos);
+        Vector3 screenPlayerPos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 relativePos = mousePos - screenPlayerPos;
+        float angle = Mathf.Atan2((float)((int)relativePos.y), (float)((int)relativePos.x)) * Mathf.Rad2Deg;
+        angle += 90;
+        weapon.SendMessage("GetAngle", (float) ((int) -angle)); // send the angle value to the weapon container
+
+        if (currentState == PlayerState.walk) {
+			UpdateAnimationsAndMove();
+		}
+	}
+
+	void UpdateAnimationsAndMove() {
+		// move
+		Vector3 change = Vector3.zero;
+		change.x = Input.GetAxisRaw("Horizontal");
+		change.y = Input.GetAxisRaw("Vertical");
+
+        Vector3 horizontalMovement = new Vector3(speed * Time.deltaTime * change.x, 0, 0);
+        Vector3 verticalMovement = new Vector3(0, 0, speed * Time.deltaTime * change.y);
+        rigidBody.MovePosition(transform.position + horizontalMovement + verticalMovement);
+
+        // change animation
+		if (change != Vector3.zero){
+			animator.SetFloat("moveX", change.x);
+			animator.SetFloat("moveY", change.y);
+			animator.SetBool("moving", true);
+		} else {
+			animator.SetBool("moving", false);
+		}
+	}
+
+	// private IEnumerator AttackCo() {
+	// 	animator.SetBool("attacking", true);
+	// 	currentState = PlayerState.attack;
+	// 	yield return null;
+	// 	animator.SetBool("attacking", false);
+	// 	yield return new WaitForSeconds(.3f);
+	// 	currentState = PlayerState.walk;
+	// }
+
+}
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public float rotateSpeed = 8f;
+
+    private float angle = 0f;
+    private Vector3 playerPosition;
+
+    private GameObject weaponRenderer;
+
+    void Start()
+    {
+        weaponRenderer = transform.GetChild(0).transform.gameObject;
+    }
+
+    void Update()
+    {
+        // set rotation to mouse location
+        Vector3 temp = new Vector3(transform.localEulerAngles.x, angle, transform.localEulerAngles.z);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(temp), Time.deltaTime * rotateSpeed);
+
+        // set rotation of sprite renderer's y to opposite of this object's rotation y
+        weaponRenderer.SendMessage("SetYAngle", -transform.localEulerAngles.y);
+    }
+
+    void StartAttackAnimation()
+    {
+        float angleSword = Mathf.Abs(angle - 90) + 45;
+        weaponRenderer.SendMessage("AttackAnimation", angleSword);
+    }
+
+    // called by player controller to pass mouse angle
+    void GetAngle(float newAngle) 
+    {
+        this.angle = newAngle;
+    }
+
+    // public float radius = 0.6f;
+    // public float height = 0.2f;
+    /* Previous method of rotation - left for reference
+    void FixedUpdate()
+    {
+        playerPosition = transform.parent.position;
+        Vector3 offset = new Vector3(Mathf.Cos(angle), height, Mathf.Sin(angle)) * radius;
+        transform.position = playerPosition + offset;
+
+        // if you want LERPed movement (causes issues)
+        //transform.position = Vector3.Lerp(transform.position, playerPosition + offset, rotateSpeed * Time.deltaTime);
+    } 
+    */
+}
