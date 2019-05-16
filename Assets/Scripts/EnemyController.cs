eb85262921c680fed72d8658c1a9e2ab882306dc
Josh Tanke <jtanke@uoregon.edu>
2019-05-15 23:02:00 -0700

    enemy animation and tracking

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,31 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+
+    public GameObject target;
+    public float stopDistance = 1.0f;
+
+    private NavMeshAgent navMeshAgent;
     private Animator anim;
 
     void Start()
     {
-        anim = GetComponentInChildren<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.stoppingDistance = stopDistance;
 
+        // Remove this and only detect player collisions
+        this.gameObject.GetComponent<Rigidbody>().detectCollisions = false;
+
+        anim = GetComponentInChildren<Animator>();   
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        navMeshAgent.SetDestination(target.transform.position);
         
+        if (navMeshAgent.velocity == Vector3.zero) {
+            anim.SetBool("enemyMoving", false);
+        } else {
+            anim.SetBool("enemyMoving", true);
+        }
+        anim.SetFloat("moveX", navMeshAgent.velocity.x);
     }
 
     public void Death() {
-    	anim.SetBool("isDed", true);
-    	this.gameObject.GetComponent<Rigidbody>().detectCollisions = false;
-    	StartCoroutine(deathCo());
+        anim.SetBool("isDed", true);
+        this.gameObject.GetComponent<Rigidbody>().detectCollisions = false;
+        StartCoroutine(deathCo());
     }
 
     IEnumerator deathCo() {
-    	yield return new WaitForSeconds(1.5f);
-    	this.gameObject.SetActive(false);
+        yield return new WaitForSeconds(1.5f);
+        this.gameObject.SetActive(false);
     }
 }
