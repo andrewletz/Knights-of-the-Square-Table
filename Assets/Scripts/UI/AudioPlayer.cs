67ce7f754a1a8ae7d12f737d53331040259b2374
andrewletz <andrewletz@gmail.com>
2019-05-31 11:13:01 -0700

    Added random music looper for dungeon

---
+++ Assets/Scripts/UI/AudioPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] clips;
+
+    void Start()
+    {
+        StartCoroutine(LoopClips());
+    }
+
+    public IEnumerator LoopClips()
+    {
+        int rand = Random.Range(0, clips.Length);
+        audioSource.clip = clips[rand];
+        audioSource.Play();
+
+        // wait for file to finish
+        yield return new WaitForSeconds(audioSource.clip.length);
+
+        // restart loop function
+        StartCoroutine(LoopClips());
+    }
+}
